8ee9903d9bd1b8e30c8f222fd21337dbbe9e5d73
HOIBOI <ahabdmer@gmail.com>
2025-03-27 10:31:19 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/react-in-jsx-scope */
+import logo from './logo.svg';
+import './App.css';
+// You can even remove the import for React if you're using the new JSX transform in React 17+
+// import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
